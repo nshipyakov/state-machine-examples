05cc6da106cfc88f2b1fd9346a35a49ddd6abfd7
Shipyakov Nikolay Dmitrievich <n.shipyakov@tinkoff.ru>
2020-10-17 12:52:41 +0300

    tinder-state-machine: delete unused companion object

--- tinder-delivery-process/src/main/kotlin/ru/tcs/deliveryprocess/statemachine/State.kt
+++ tinder-delivery-process/src/main/kotlin/ru/tcs/deliveryprocess/statemachine/State.kt
@@ -6,13 +6,4 @@ sealed class State(val timerValueInSeconds: Long = 0) {
     object ReadyToSend: State()
     object WaitingOrder: State(20)
     object End: State()
-
-    companion object {
-
-        fun find(name: String): State {
-            return checkNotNull(State::class.sealedSubclasses.first{
-                it.simpleName == name
-            }.objectInstance)
-        }
-    }
 }
